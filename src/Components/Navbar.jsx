023575baa6b46e2993d0b6025ca31c11d0af06e0
avanish-123 <avanishpandey022@gmail.com>
2024-04-09 16:18:54 +0530

    working on navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,28 +1,52 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import logo from "../assets/Logo/Logo-Full-Light.png";
 import smallLogo from "../assets/Logo/Logo-Small-Light.png";
 import AuthButton from "./AuthButton";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, matchPath, useLocation } from "react-router-dom";
 import { NavbarLinks } from "../data/navbar-links";
 import { FaAngleDown } from "react-icons/fa";
 import SubMenu from "./SubMenu";
+import { useSelector } from "react-redux";
+import { apiConnector } from "../Services/apiconnector";
+import { category } from "../Services/apilist";
+import toast from 'react-hot-toast'
 
 export const Navbar = () => {
   const navigate = useNavigate();
+  const location = useLocation();
+  const [courseCategories, setCoursecategories] = useState([]);
   const [visible, setVisible] = useState(false);
-  function navbarClickHandler (element) {
-    if(!Object.keys(element).includes('hasDropDown')){
-      navigate(`${element.path}`)
+  const { token } = useSelector((state) => state.auth);
+  function navbarClickHandler(element) {
+    if (!Object.keys(element).includes("hasDropDown")) {
+      navigate(`${element.path}`);
     }
   }
-  function navSubManuMouseEnter(element){
-    if(Object.keys(element).includes('hasDropDown')){
-      setVisible(true)
+  function navSubManuMouseEnter(element) {
+    if (Object.keys(element).includes("hasDropDown")) {
+      setVisible(true);
     }
   }
-  function navSubManuMouseLeave(element){
-      setVisible(false)
+  function navSubManuMouseLeave(element) {
+    setVisible(false);
   }
+
+  function matchRoute(route) {
+    return matchPath({ path: route }, location.pathname);
+  }
+
+  const getCategories = useCallback(async () => {
+    try {
+      const result = await apiConnector("GET", category.CATEGORY_API);
+      setCoursecategories(result.data.data);
+    } catch (error) {
+      toast.error(error.message)
+    }
+  }, []);
+
+  useEffect(() => {
+    getCategories();
+  }, []);
 
   return (
     <div className="border-b-[1px] border-b-richblack-400 h-12 w-full fixed bg-richblack-900 z-[100]">
@@ -34,16 +58,25 @@ export const Navbar = () => {
           </Link>
         </div>
         <div className="text-white flex items-center justify-center max-sm:hidden">
-          <ul onMouseLeave={()=>navSubManuMouseLeave()} className="flex items-center justify-center gap-4 text-sm">
+          <ul
+            onMouseLeave={() => navSubManuMouseLeave()}
+            className="flex items-center justify-center gap-4 text-sm"
+          >
             {NavbarLinks.map((ele, index) => (
               <li
-                onClick={()=>navbarClickHandler(ele)}
-                onMouseEnter={()=>navSubManuMouseEnter(ele)}
+                onClick={() => navbarClickHandler(ele)}
+                onMouseEnter={() => navSubManuMouseEnter(ele)}
                 key={index}
-                className="flex gap-[2px] py-2 cursor-pointer relative"
+                className={`${
+                  !ele.hasDropDown
+                    ? matchRoute(ele?.path)
+                      ? "text-yellow-25"
+                      : "text-pure-greys-5"
+                    : ""
+                } flex gap-[2px] py-2 cursor-pointer relative`}
               >
                 {ele.title}
-                {ele.hasDropDown ? (
+                {ele?.hasDropDown ? (
                   <div className="flex items-center">
                     <FaAngleDown />
                   </div>
@@ -52,21 +85,32 @@ export const Navbar = () => {
                 )}
               </li>
             ))}
-                  <div className="absolute top-14 left-[35rem]">
-                    <SubMenu data={""} visible={visible} setVisible={setVisible} />
-                  </div>
+            <div className="absolute top-14 left-[35rem]">
+              <SubMenu
+                data={courseCategories}
+                visible={visible}
+                setVisible={setVisible}
+              />
+            </div>
           </ul>
         </div>
-        <div className="flex items-center gap-2">
-          <AuthButton
-            label={"Login"}
-            onclick={() => navigate("/login")}
-          ></AuthButton>
-          <AuthButton
-            label={"Signup"}
-            onclick={() => navigate("/signup")}
-          ></AuthButton>
-        </div>
+        {!token ? (
+          <div className="flex items-center gap-2">
+            <AuthButton
+              label={"Login"}
+              onclick={() => navigate("/login")}
+            ></AuthButton>
+            <AuthButton
+              label={"Signup"}
+              onclick={() => navigate("/signup")}
+            ></AuthButton>
+          </div>
+        ) : (
+          <div>
+            <AuthButton label={"DashBoard"} />
+            <AuthButton label={"Logout"} />
+          </div>
+        )}
       </div>
     </div>
   );
--- src/Components/SubMenu.jsx
+++ src/Components/SubMenu.jsx
@@ -4,16 +4,22 @@ export default function SubMenu(props) {
   const { data, visible, setVisible } = props;
   return (
     visible && (
-        <><div className="bg-pure-greys-5 w-36 h-max max-h-[70vh] overflow-auto relative rounded-xl">
-        <div className="p-3 text-richblack-800 z-[3]">
-          <div
-            onClick={() => setVisible(false)}
-            className="rounded-md hover:bg-pure-greys-600 hover:text-pure-greys-5 px-1"
-          >
-            hello
+      <>
+        <div className="bg-pure-greys-5 w-36 h-max max-h-[70vh] overflow-auto relative rounded-xl">
+          <div className="p-3 text-richblack-800 z-[3]">
+            {data.map((ele, index) => (
+              <div
+                key={index}
+                onClick={() => setVisible(false)}
+                className="rounded-md hover:bg-pure-greys-600 hover:text-pure-greys-5 px-1"
+              >
+                {ele?.name}
+              </div>
+            ))}
           </div>
         </div>
-      </div><div className="border-l-[20px] border-l-transparent border-r-transparent border-b-pure-greys-5 border-r-[20px] border-b-[30px] absolute -top-[22px] left-[100px]"></div></>
+        <div className="border-l-[20px] border-l-transparent border-r-transparent border-b-pure-greys-5 border-r-[20px] border-b-[30px] absolute -top-[22px] left-[100px]"></div>
+      </>
     )
   );
 }
